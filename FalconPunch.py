bcb0d73a43f83b59122eecd187278384f39d075b
Rain Clark <bread.melon@gmail.com>
2015-04-28 22:03:21 -0400

    Added IP Dialog & Sent Confirmation

--- FalconPunch.py
+++ FalconPunch.py
@@ -1,9 +1,10 @@
-import os, socket, sys, struct
+#!/usr/bin/env python
+import os, socket, sys, struct, easygui
 
 statinfo = os.stat(sys.argv[1])
 fbiinfo = struct.pack('!q', statinfo.st_size)
 p = sys.argv[1]
-dsip = raw_input('Enter IP: ')
+dsip = easygui.enterbox("Enter 3DS' IP:", "FalconPunch", "192.168.1.1")
 
 file = open(p, "rb")
 sock = socket.socket()
@@ -14,6 +15,7 @@ sock.send(fbiinfo)
 while True:
     chunk = file.read(16384)
     if not chunk:
+        easygui.msgbox("Sent " + p + " to the 3DS", "FalconPunch")
         break  # EOF
     sock.sendall(chunk)
 
